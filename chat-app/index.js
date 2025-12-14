c6e0244fe30f1e58cac43efd2d62708f3296abde
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-12-14 23:37:57 +0530

    done with all setup

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const {Server} = require("socket.io");
+const dotenv =  require('dotenv');
+connectDB = require('./config/db');
+// const http = require('http');
+// const {Server} = require("socket.io");
 
 const file = path.resolve("./public/index.html")
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server)
+dotenv.config();
+connectDB();
 
-io.on('connection', (socket) => { // socket => client
-    socket.on('user-message-client', (message) => {
-        io.emit('user-message-server', message)
-    })
-})
+// const server = http.createServer(app);
+// const io = new Server(server)
+
+// io.on('connection', (socket) => { // socket => client
+//     socket.on('user-message-client', (message) => {
+//         io.emit('user-message-server', message)
+//     })
+// })
 
 app.get('/', (req, res) =>{
     res.sendFile(file);
@@ -23,5 +28,5 @@ app.post('/signin', (req, res) =>{
     
 })
 
-server.listen(9000, () =>{console.log("server is started at port 9000")});
+app.listen(3000, () =>{console.log("server is started at port 9000")});
 
